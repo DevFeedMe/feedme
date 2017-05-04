3d63910c8cabcbb32cdd777d19e9768aba60117f
MacBookPro <macbookpro@macbook-jourath.local>
2017-05-04 22:30:59 +0100

    Initial Commit

---
+++ feedmeTests/feedmeTests.swift
@@ -0,0 +1,36 @@
+//
+//  feedmeTests.swift
+//  feedmeTests
+//
+//  Created by MacBookPro on 04/05/2017.
+//  Copyright © 2017 MacBookPro. All rights reserved.
+//
+
+import XCTest
+@testable import feedme
+
+class feedmeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
